fa385d48d593131c4b7abc545886a9c83bbcad3e
Jose Maria Vences <jose.vencar.vences@gmail.com>
2022-05-14 20:39:04 -0500

    Configurando el modulo github

--- src/github/github.module.ts
+++ src/github/github.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { GithubController } from './github.controller';
+import { GithubService } from './github.service';
 
-@Module({})
+@Module({
+    imports: [],
+    controllers: [ GithubController ],
+    providers: [ GithubService ] 
+})
 export class GithubModule {}
