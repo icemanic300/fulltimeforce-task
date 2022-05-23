fb1875408920a39c4e30f53a78b101b99e46865b
Jose Maria Vences <jose.vencar.vences@gmail.com>
2022-05-22 23:30:16 -0500

    Primer endpoint correcto

--- src/github/github.controller.ts
+++ src/github/github.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { GithubService } from './github.service';
 
 @Controller('github')
-export class GithubController {}
+export class GithubController {
+
+    constructor(
+        private gitHubService: GithubService
+    ) {}
+
+    @Get('')
+    getRepos() {
+        return "Prueba de Modulo GIT";
+    }
+
+    @Get('/repos')
+    async getReposByUsername( @Param('username') username: string) {
+        let data = await this.gitHubService.getReposByUsername(username)
+        return data.data;
+    }
+
+}
--- src/github/github.service.ts
+++ src/github/github.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class GithubService {}
+export class GithubService {
+
+    async getReposByUsername(username: string) {
+        return await axios({
+            method: 'get',
+            url: 'https://api.github.com/users/icemanic300/repos',
+            headers: {
+                'Content-Type': 'aplication/json'
+            }
+        })
+    }
+
+}
